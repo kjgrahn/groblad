b7b8449b220abecc9fd851b0e68f8c5d08674255
grahn <grahn>
2007-09-26 21:22:46 +0000

    hide behind __main__

--- groblad_report.py
+++ groblad_report.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 #
-# $Id: groblad_report.py,v 1.7 2007-09-26 21:16:34 grahn Exp $
+# $Id: groblad_report.py,v 1.8 2007-09-26 21:22:46 grahn Exp $
 #
 # Copyright (c) 2004, 2005, 2007 Jörgen Grahn <jgrahn@algonet.se>
 # All rights reserved.
@@ -76,100 +76,101 @@ class Point:
         return ' '.join(acc)
 
 
-lines = []
-headerre = re.compile(r'^(\w+)\s*:\s*(.+)')
-plantre1 = re.compile(r'^(.+?)\s*:\S:\s*(.*)')
-plantre2 = re.compile(r'^(.+?)\s*:.:\s*(.+)')
+if __name__ == "__main__":
+    lines = []
+    headerre = re.compile(r'^(\w+)\s*:\s*(.+)')
+    plantre1 = re.compile(r'^(.+?)\s*:\S:\s*(.*)')
+    plantre2 = re.compile(r'^(.+?)\s*:.:\s*(.+)')
 
-for s in fileinput.input():
-    s = s.rstrip()
-    if s=='': continue
-    if s[0]=='#': continue
-    if s[0] in ' \t':
-        lines[-1] = lines[-1] + ' ' + s.strip()
-    else:
-        lines.append(s)
+    for s in fileinput.input():
+        s = s.rstrip()
+        if s=='': continue
+        if s[0]=='#': continue
+        if s[0] in ' \t':
+            lines[-1] = lines[-1] + ' ' + s.strip()
+        else:
+            lines.append(s)
 
-places = []
-place = None
-plants = None
-seen = {}
+    places = []
+    place = None
+    plants = None
+    seen = {}
 
-for s in lines:
-    if s=='{':
-        place = Place()
-        continue
-    if s=='}':
-        places.append(place)
-        place = None
-        plants = None
-        continue
-    if s=='}{':
-        plants = place.plants
-        continue
-    if plants != None:
-        for rex in (plantre1, plantre2):
-            m = rex.match(s)
-            if m:
-                name, comment = m.groups()
-                plants[name] = comment
-                seen[name] = 1
-        continue
-    m = headerre.match(s)
-    if m:
-        name, value = m.groups()
-        if name=='place':
-            place.place = value
-        elif name=='coordinate':
-            try:
-                place.coordinate = apply(Point, map(int, value.split()))
-            except ValueError:
-                print >>sys.stderr, 'bad coordinate', value, 'ignored'
-        elif name=='date':
-            place.date = value
-        elif name=='observers':
-            place.observers = value
-        elif name=='comments':
+    for s in lines:
+        if s=='{':
+            place = Place()
+            continue
+        if s=='}':
+            places.append(place)
+            place = None
+            plants = None
+            continue
+        if s=='}{':
+            plants = place.plants
+            continue
+        if plants != None:
+            for rex in (plantre1, plantre2):
+                m = rex.match(s)
+                if m:
+                    name, comment = m.groups()
+                    plants[name] = comment
+                    seen[name] = 1
+            continue
+        m = headerre.match(s)
+        if m:
+            name, value = m.groups()
+            if name=='place':
+                place.place = value
+            elif name=='coordinate':
+                try:
+                    place.coordinate = apply(Point, map(int, value.split()))
+                except ValueError:
+                    print >>sys.stderr, 'bad coordinate', value, 'ignored'
+            elif name=='date':
+                place.date = value
+            elif name=='observers':
+                place.observers = value
+            elif name=='comments':
+                pass
+            else:
+                print >>sys.stderr, 'hey, what\'s this?', s
+        else:
             pass
-        else:
-            print >>sys.stderr, 'hey, what\'s this?', s
-    else:
-        pass
 
-species = []
-speciesre = re.compile(r'^(.+?)\s*\((.+)\)')
-f = open('INSTALLBASE/lib/groblad/species', 'r')
-for s in f.readlines():
-    m = speciesre.match(s)
-    if m:
-        trivial, latin = m.groups()
-        species.append(Species(trivial, latin))
+    species = []
+    speciesre = re.compile(r'^(.+?)\s*\((.+)\)')
+    f = open('INSTALLBASE/lib/groblad/species', 'r')
+    for s in f.readlines():
+        m = speciesre.match(s)
+        if m:
+            trivial, latin = m.groups()
+            species.append(Species(trivial, latin))
 
-for sp in species:
-    name = sp.trivial
-    if not seen.has_key(name): continue
-    del seen[name]
-    print '.XP'
-    print '%s:' % sp
-    ss = []
-    for p in places:
-        if not p.contains(name): continue
-        if p.coordinate:
-            s = '%s\n(%s).' % (p.place, p.coordinate.tstr())
-        else:
-            s = '%s.' % p.place
-        if p.date and p.observers:
-            s += '\n%s %s.' % (p.observers, p.date)
-        if p.plants[name] != '':
-            s += '\n%s' % p.plants[name]
-        if s[-1] != '.':
-            s += '.'
-        ss.append(s)
-    print '\n\\(em\n'.join(ss)
-    print '.'
+    for sp in species:
+        name = sp.trivial
+        if not seen.has_key(name): continue
+        del seen[name]
+        print '.XP'
+        print '%s:' % sp
+        ss = []
+        for p in places:
+            if not p.contains(name): continue
+            if p.coordinate:
+                s = '%s\n(%s).' % (p.place, p.coordinate.tstr())
+            else:
+                s = '%s.' % p.place
+            if p.date and p.observers:
+                s += '\n%s %s.' % (p.observers, p.date)
+            if p.plants[name] != '':
+                s += '\n%s' % p.plants[name]
+            if s[-1] != '.':
+                s += '.'
+            ss.append(s)
+        print '\n\\(em\n'.join(ss)
+        print '.'
 
-# At this point 'seen' may contain a number of names
-# which aren't good species. Print them as a warning.
+    # At this point 'seen' may contain a number of names
+    # which aren't good species. Print them as a warning.
 
-for name in seen.keys():
-    print >>sys.stderr, 'unknown species:', name
+    for name in seen.keys():
+        print >>sys.stderr, 'unknown species:', name
